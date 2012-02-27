f1b68ece680a767555513b612b81fcc3f54ec6d7
Mark L. Chang <mark.chang@gmail.com>
2012-02-26 22:48:24 -0500

    as per jffry

--- v2/_framework/lib/BanSubdomain.php
+++
@@ -1,9 +0,0 @@
-<?php
-if($_SERVER['HTTP_HOST']!='acl.olin.edu'){
-	$protocol = ($_SERVER['HTTPS']=='on'?'https':'http');
-	$url = "$protocol://acl.olin.edu/directory".$_SERVER['PHP_SELF'];
-	$url = preg_replace('/\/index.php$/','/',$url);
-	header("Location: $url");
-	die("Please update your bookmarks to use this URL: <a href=\"$url\">$url</a>");
-	}
-?>
--- v2/account/index.php
+++
@@ -1,4 +0,0 @@
-<?php header("Location: ../"); ?>
-<h2>OlinDirectory</h2>
-<b>Oops!</b>
-It looks like you ended up somewhere you shouldn't have.  Do you want to <a href="./edit/">edit your account</a> or <a href="../">return home</a>?
